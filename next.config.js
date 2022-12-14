f87c9e43ee0638b14ae62cd1360fc8cf94e80939
dracero <diego.racero@gmail.com>
2022-12-14 19:46:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+/*const nextConfig = {
+  reactStrictMode: true,
+}*/
+
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api.example.com/:path*',
+          },
+        ]
+      },
+  };
+
+//module.exports = nextConfig
