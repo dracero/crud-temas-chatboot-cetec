3ee65971cbf0a0c092343ad072e3a524fd9d6b7c
dracero <diego.racero@gmail.com>
2022-12-14 19:59:10 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
+//const nextConfig = {
+//  reactStrictMode: true,
+//}
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://crud-temas-chatboot-cetec-p5pxmvjnl-diegoracero.vercel.app/:path*'
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+//module.exports = nextConfig
